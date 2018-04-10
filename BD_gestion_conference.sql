f5a5ff9a8a2839f50d0385e7aad2cfdb15e69f4e
said el farkh <saelfarkh@MPPORT-LNH-040.lnh.local>
2018-04-10 10:33:47 +0200

    modification bd

--- BD_gestion_conference.sql
+++ BD_gestion_conference.sql
@@ -56,3 +56,24 @@ CREATE TABLE suivre (
   CONSTRAINT FK_suivre_CONFERENCE FOREIGN KEY (id_conference) REFERENCES CONFERENCE(id_conference),
   CONSTRAINT FK_suivre_membre FOREIGN KEY (id_membre) REFERENCES membre(id_membre)
 );
+
+insert into membre(nom,prenom,email,tel,login,mdp,est_admin) values
+("elfarkh","said","said@gmail.com","0656457851","user","user",0),
+("elfarkh","younes","younes@gmail.com","0656457851","admin","admin",1);
+
+insert into conference(theme ,date_debut ,date_fin , lieu , ville ,pays , site_web, description , est_valide ) values 
+("theme1",2000-04-12,2000-04-12,"paris","universite","france","www.theme1.fr","blablablablabbbbb",1),
+("theme2",2000-04-12,2000-04-12,"clermont","universite","france","www.theme2.fr","blablablablabbbbb",1),
+("theme3",2000-04-12,2000-04-12,"jaude","universite","france","www.theme3.fr","blablablablabbbbb",1),
+("theme4",2000-04-12,2000-04-12,"lyon","universite","france","www.theme4.fr","blablablablabbbbb",1);
+
+
+insert into  conferencier( nom , prenom , fonction , email ,tel ,  site_web ) values 
+("elùmkoudi","anass","directeur","anas@gmail.com","0624518495","www.theme.fr"),
+("elfarkh","said","directeur","said@gmail.com","0624518495","www.theme.fr"),
+("minouar","hamza","directeur","hamza@gmail.com","0624518495","www.theme.fr"),
+("bouchefra","khalil","directeur","anakhalils@gmail.com","0624518495","www.theme.fr");
+
+insert into animer values (1,1),(2,2),(3,3),(4,4);
+
+insert into suivre values(1,1),(1,2),(2,1),(2,2),(3,1),(3,2),(4,1),(4,2);
--- models/signUp.php
+++ models/signUp.php
@@ -7,17 +7,13 @@
 function register($con , $login , $mdp , $nom , $prenom,$email , $tel){
 
   
-  
+  //verification si le login existe deja
   $str = "select * from membre where login = '".$login."'";
   $req = mysqli_query($con,$str);
   $res = mysqli_fetch_assoc($req);
   if($res) return false;
   
   $hash = password_hash($mdp,PASSWORD_BCRYPT) ;
-  
- 
-
-
   $str = "insert into membre set nom = '".$nom."' ,prenom = '".$prenom."' ,email = '".$email."' , tel = '".$tel."' , login = '".$login."' , mdp = '".$hash."'";
   $req = mysqli_query($con,$str);
   
